18461bb716cc8516a15d8e7b09ca0c739f2741b8
Adam D. Moss <c@yotes.com>
2021-08-04 19:20:37 -0700

    add some colors from the 256-color palette, highlight STRINGS_LIKE_THIS

--- colorit.sh
+++ colorit.sh
@@ -28,6 +28,11 @@ dim="${ESC}[02m"
 printf -v bell "\007"
 frame="${ESC}[53;04m"
 
+brightblue="${blue}${ESC}[38;5;111m"
+# ^ sort of lilac for 256-color terminals
+brightyellow="${yellow}${ESC}[38;5;215m"
+# ^ sort of golden for 256-color terminals
+
 Normal="${ESC}[0m"
 reset_italic="${ESC}[23m"
 reset_underline="${ESC}[24m"
@@ -40,8 +45,8 @@ reset_frame=${reset_underline}"${ESC}[55m"
 reset_bell=${bell}
 
 # ugh, 'watch -c' doesn't appear to obey 39m, use full reset
-#reset_foreground="${ESC}[39m"
-reset_foreground=${Normal}
+reset_foreground="${ESC}[39m"
+#reset_foreground=${Normal}
 reset_green=${reset_foreground}
 reset_red=${reset_foreground}${reset_bold}
 reset_yellow=${reset_foreground}
@@ -51,11 +56,14 @@ reset_blue=${reset_foreground}${reset_bold}
 reset_black=${reset_foreground}${reset_bold}
 reset_white=${reset_foreground}
 
-error=${red}${blink}${frame}
-reset_error=${reset_red}${reset_error}${reset_blink}${reset_frame}
+error="${bold}${red}${frame}"
+reset_error="${reset_frame}${reset_red}${reset_bold}"
 
 warning=${red}
 reset_warning=${reset_red}
+
+reset_brightblue="${Normal}"
+reset_brightyellow="${Normal}"
 
 a_lookaheads=()
 a_matchstrings=()
@@ -65,7 +73,9 @@ a_colorendnames=()
 a_lookbehindcaptures=()
 
 #escmatch=${ESC}'\[[0-9;]+m'
-escmatch=.'\[[0-9;]+m'
+#escmatch=.'\[[0-9;]+m'
+escmatch=''${ESC}'\[[0-9;]+m'
+# ^ maybe use one above this line if any problems
 #zeroesc=$(printf "\(${escmatch}\)\{0\}")
 
 function colorit {
@@ -77,7 +87,7 @@ function colorit {
         #colorit '^((\[[[:space:]0-9.]+\][[:space:]]*)|([[:alnum:]_-.]+:)){1,2}' cyan
         #colorit '^((\[[[:space:]0-9.]+\][[:space:]]*)|( *[[:alnum:]_-.]+(\[[[:digit:]]+\])?:)){1,2}' cyan
         #colorit '^((\[[[:space:]0-9.]+\][[:space:]]*)|( *[[:alnum:]\_\-\.]+(\[[[:digit:]]+\])?:)){1,2}' cyan
-        colorit '^((\[[[:space:]0-9.]+\][[:space:]]*)|(( ?[-[:alnum:]_\.])+(\[[[:digit:]]+\])?:)){1,3}' cyan
+        colorit '^' '((\[[[:space:]0-9.]+\][[:space:]]*)|(:?( ?[-[:alnum:]_\.])+(\[[[:digit:]]+\])?)){1,3}' ':[^\\]' cyan
         # ^ 'fieldname[1]: foo' or '[num.num]... (for dmesg timestamps etc)
         colorit '^|[[:space:](@]' '((Mon|Tue|Wed|Thu|Fri|Sat|Sun)[[:space:]]{1,2}[1,2,3]?[[:digit:]]?[[:space:]]?)?(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[[:space:]]{0,2}[123]?[[:digit:]]?[[:space:]]{1,2}([012]?[[:digit:]](:[0-5][[:digit:]]){1,2}[[:space:]]?|[12][90][[:digit:]]{2}[[:space:]]?){1,2}|20[[:digit:]]{2}-[01][0-9]-[0123][0-9]|19[[:digit:]]{2}-[01][0-9]-[0123][0-9]|20[[:digit:]]{2}\/[01]?[0-9]\/[0123]?[0-9]|19[[:digit:]]{2}\/[01]?[0-9]\/[0123]?[0-9]|[01]?[0-9]\/[0123]?[0-9]\/20[[:digit:]]{2}|[01]?[0-9]\/[0123]?[0-9]\/19[[:digit:]]{2}' '\)|[[:space:]]?|$' green
         # ^ long-form date/time.  not really in love with how this still gets processed by the below stand-alone numbers line
@@ -88,16 +98,17 @@ function colorit {
         #colorit '^|[[:space:]]|[^[:alnum:][:cntrl:].:;_,]' '[0-9]{1,3}(,[0-9]{3}){1,}|0x[a-f0-9]+|#?0|([a-f0-9]{2} ?){4,}|#?[1-9][0-9]*(\.[0-9]+){0,1}' '[^[:alnum:][:cntrl:].;:_,]|[[:space:]]|$' dim # frustratingly doesn't highlight final hex pair if spaced
         #colorit '^|[[:space:],]|[^[:alnum:][:cntrl:].:;_]' '[0-9]{1,3}(,[0-9]{3})+|0x[a-f0-9]+|#?0|([a-f0-9]{2}){4,}|#?[1-9][0-9]*(\.[0-9]+){0,1}' '[^[:alnum:][:cntrl:].;:_]|[[:space:]]|$' dim # frustratingly doesn't highlight final hex pair if spaced
         #colorit '^|[[:space:],]|[^[:alnum:][:cntrl:].:;_-]' '(-?[1-9][0-9]{0,2}(,[0-9]{3})+|0|-?[0-9]|-|[#-]?[1-9][0-9]*)(\.[0-9]+)?|(0x[a-f0-9]+,?-?)+|[a-f0-9]{2}( ?[a-f0-9]{2}){3,}' ${escmatch}'|[/[:space:],)]|$' dim # without escmatch frustratingly may not highlight final hex pair if spaced?
-        colorit '^|[[:space:],:]|[^[:alnum:][:cntrl:].:;_-]' '(-?[1-9][0-9]{0,2}(,[0-9]{3})+|0|-?[0-9]|-|[#-]?[1-9][0-9]*)(\.[0-9]+)?|(0x[a-f0-9]+,?-?)+|[a-f0-9]{2}( ?[a-f0-9]{2}){3,}' ${escmatch}'|[]/[:space:],)]|$' dim # without escmatch frustratingly may not highlight final hex pair if spaced?
+        colorit '^|[[:space:],:]|[^[:alnum:][:cntrl:].:;_-]' '(-?[1-9][0-9]{0,2}(,[0-9]{3})+|0|-?[0-9]|-|[#-]?[1-9][0-9]*)(\.[0-9]+)?|(0x[a-f0-9]+,?-?)+|[a-f0-9]{2}( ?[a-f0-9]{2}){3,}' "${escmatch}"'|[]/[:space:],)]|$' dim # without escmatch frustratingly may not highlight final hex pair if spaced?
         # ^ general stand-alone numbers
         #colorit '^|[^[:alnum:][:cntrl:][:space:].:;_]|[[:space:]]' '[a-f0-9]{2}( ?[a-f0-9]{2}){2,}[a-f0-9]{2}' '[^[:alnum:][:cntrl:][:space:].;:_]|[[:space:]]|$' red
         ###colorit '^|[[:space:]]' '[0-9]+(\.[0-9]+){0,1}' '[[:space:]]|$' dim
         #colorit '^|[[:space:]]|[^[:alnum:][:cntrl:].:;_]' '0x[a-f0-9]+|#?0|[a-f0-9]{2}{4,}|#?[1-9][0-9]*(\.[0-9]+){0,1}' '[^[:alnum:][:cntrl:].;:_]|[[:space:]]|$' dim
         # ^ general stand-alone numbers
-        colorit '^|[[:space:]]|'${escmatch} '(\.\.|\.[^[:space:]./][^[:space:]/]*)' '[[:space:]]|$' dim
+        colorit '^|[[:space:]]|'"${escmatch}" '(\.\.|\.[^[:space:]./][^[:space:]/]*)' '[[:space:]]|$' dim
         # ^ .. or .foo
         #colorit '^|[^[:cntrl:]]' '\/\/.*|(\[.{1,}\])|(<.{1,}>)|(\(.{1,}\))|(\{.{1,}\})' '' italic
-        colorit '[^'${ESC}']' '\[[^][]+\]|<[^<>]+>|\([^\(\)]+\)|\{[^{}]+\}' '' italic
+        colorit '[^a-z]\(' '[^\(\)]+' '\)' dim
+        colorit '[^'${ESC}']' '\[[^][]+\]|<[^<>]+>|\{[^{}]+\}' '' dim
         # ^ things in various brackets
         colorit '^[[:space:]]*' '#.*|\/\/.*' '' dim
         # ^ single-line comments
@@ -116,8 +127,15 @@ function colorit {
         # ^ ipv4
         colorit '[[:space:]]|^' '([0-9a-f]{2,4}:+){4,7}[0-9a-f]{1,4}' '[[:space:]]|$' yellow
         # ^ ipv6
-        colorit '[(=,[:space:]]|^' '(`[^`]*`)|('\''[^'\'']*'\'')|(L?)"(\\"|[^"])*"' '[]),[:space:]]|$' yellow
-        # ^ L"string" or "string" or "str\"ing" or `string`
+        colorit '[(=,[:space:]]|^' '(`[^`]*`)|('\''[^'\'']*'\'')|(L?)"(\\"|[^"])*"|([a-z_][[:alnum:]_]*::)+[a-z_][[:alnum:]_]*' '[]),'${ESC}'[:space:]]|$' brightyellow
+        # ^ L"string" or "string" or "str\"ing" or `string` ORRRR foo::bar enum name
+        colorit '[[:space:]]|^' '[a-z_][[:alnum:]_:]*' '\(([^s\)][^\)]*|s[^\)]+)?\)' green
+        # ^ function name, but not foo(s) plural text form
+        #colorit "${escmatch}"'[a-z_][[:alnum:]_:]*'"${escmatch}"'\(' '[a-z_][[:alnum:]_]*' '\)' underline
+        colorit '\b|[|&]' '[a-z]([[:alnum:]]*_[[:alnum:]]+)+' ${ESC}'|[[:space:],|&)]|$' brightblue
+        # ^ FOO_BAR_BAZ (e.g. enum)
+        colorit "${escmatch}"'[a-z_][[:alnum:]_:]*'"${escmatch}"'\(' '[^\)\(]+' '\)' cyan
+        # ^ general params after function name (incl. escseq), overkill?
         return
     fi
     
@@ -140,13 +158,23 @@ function colorit {
     
     # count non-literal parentheses in matchstring so we know which regex capture numbers to use :(
     
-    brackets=${matchstring//[!(]}
+    #matchstring='a(b(c(d\(\x'
+    #printf '%s\n' $matchstring
+    brackets=${matchstring}
+    brackets=${matchstring//\\(} # remove \(
+    #printf '%s\n' $brackets
+    brackets=${brackets//[!(]} # leave (
+    #printf '%s\n' $brackets
     bracketcount=${#brackets}
+    #printf '%s\n' $bracketcount
     #bracketcount=$(printf '%s' "${matchstring}" | grep -F -o "(" | wc -l)
     
-    litbrackets="${matchstring//!(*\\\(*)/X}" # note: generates one too many 'X's
-    litbracketcount=${#litbrackets}
-    (( lookbehindcapture = ${bracketcount} - ${litbracketcount} + 5 ))
+    #litbrackets="${matchstring//!(*\\\(*)/X}" # note: generates one too many 'X's
+    #litbracketcount=${#litbrackets}
+    #(( lookbehindcapture = ${bracketcount} - ${litbracketcount} + 5 ))
+
+    (( lookbehindcapture = ${bracketcount} + 4 ))
+
     [[ ${lookbehindcapture} -gt 9 ]] && echo 'regex has too many nested captures :( - '"${matchstring}" && exit 1
 
     #litbracketcount=$(printf '%s' "${matchstring}" | grep -F -o '\(' | wc -l)
